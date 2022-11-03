499fda49a912e8ed592252cba17fe1f3ed3ebf70
humanplayer2 <rendsvig@gmail.com>
2022-11-03 14:37:05 +0100

    binary matrix suggests newest Pull dir: added some pretty

--- data-processing/binary-matrix.sh
+++ data-processing/binary-matrix.sh
@@ -17,35 +17,37 @@ printf "${GREEN}
         The newest data directory I see is
         $NEWEST
                 
-        ${RED}Is that the directory of your data? (y/n)${NORMAL}
-        "
-
+        ${RED}Is that the directory of your data? (y/n)${NORMAL}\n
+        >>> "
 read ynchoice
-    case "$ynchoice" in
-      y|Y ) printf "${GREEN}
+
+case "$ynchoice" in
+  y|Y ) printf "${GREEN}
         Great, I will build the matrices for $NEWEST
-        This may take a bit!${NORMAL}"
+        This may take a bit!${NORMAL}\n"
         export NEWEST
         python3 -c "import os; from resources import processing;\
                     processing.aggegate_likers(os.getenv('NEWEST'));\
                     processing.aggegate_retweeters(os.getenv('NEWEST'))"
-      n|N ) printf '${RED}
+        printf "${MAGENTA_BLINK}\n  DONE!${NORMAL}\n\n";;
+  n|N ) printf "
         Then please supply the path of the pull data folder:${ORANGE}
-        (use "../Pull-DD-MM-YYYY-hour:minute:second"
-        for "Pull-..." in the directory above this one):${NORMAL}'
-        read CHOICE
-        if [ -d $CHOICE ]
-        then
-        printf "${GREEN}\n        Great, I see $CHOICE\n        and will build the matrices. This may take a bit!\n"
-        export CHOICE
-        python3 -c "import os; from resources import processing;\
-                processing.aggegate_likers(os.getenv('CHOICE'));\
-                processing.aggegate_retweeters(os.getenv('CHOICE'))"
-        printf "${MAGENTA_BLINK}
-        DONE!
-        "${NORMAL}
-        else
-          printf "${RED}\n        Sorry, I can't find that directory.${NORMAL}\n"
-        fi
-      * ) printf "${RED}\n        Invalid choice, I'm afraid. Quitting...${NORMAL}\n"; exit;;
+        (use ../Pull-DD-MM-YYYY-hour:minute:second
+        for Pull-... in the directory above this one)${NORMAL}\n
+        >>> "
+    read CHOICE
+    if [ -d $CHOICE ]; then
+      printf "${GREEN}
+        Great, I see $CHOICE
+        and will build the matrices. 
+        This may take a bit!${NORMAL}\n"
+      export CHOICE
+      python3 -c "import os; from resources import processing;\
+                  processing.aggegate_likers(os.getenv('CHOICE'));\
+                  processing.aggegate_retweeters(os.getenv('CHOICE'))"
+      printf "${MAGENTA_BLINK}\n  DONE!${NORMAL}\n\n"
+    else
+      printf "${RED}\n        Sorry, I can't find that directory.${NORMAL}\n"; exit;
+    fi;;
+  * ) printf "${RED}\n        Invalid choice, I'm afraid. Quitting...${NORMAL}\n"; exit;
 esac
