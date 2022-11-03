cc6861f9281ff14ae16b11e28436beffaa4dd328
humanplayer2 <rendsvig@gmail.com>
2022-11-03 13:43:29 +0100

    binary-matrix suggestes newest Pull dir.

--- data-processing/binary-matrix.sh
+++ data-processing/binary-matrix.sh
@@ -8,30 +8,44 @@ BLUE=$(tput setaf 153)
 ORANGE=$(tput setaf 130)
 MAGENTA_BLINK=$(tput blink setaf 201)
 
+NEWEST=$(ls -td ../Pull*/ | head -1)
+
 printf "${GREEN}
         So, you want to combine some data into binary
         'Who liked/retweeted what?' matrices, huh?${RED}
-        Please supply the name of the pull data folder${NORMAL}
-        (E.g. '../Pull-DD-MM-YYYY-hour:minute:second'
-        for 'Pull-...' in the directory above this one):${ORANGE}
+
+        The newest data directory I see is
+        $NEWEST
+                
+        ${RED}Is that the directory of your data? (y/n)${NORMAL}
         "
-read choice
 
-if [ -d $choice ]
-then
-  printf "${GREEN}
-        Great, I see $choice 
-        and will build the matrices. This may take a bit!\n"
-  export choice
-  python3 -c "import os; from resources import processing;\
-              processing.aggegate_likers(os.getenv('choice'));\
-              processing.aggegate_retweeters(os.getenv('choice'))"
-
-  printf "${MAGENTA_BLINK}
-          DONE!
-  "${NORMAL}
-else
-  printf "${RED}
-        Sorry, I can't find that directory.
-  "${NORMAL}
-fi
+read ynchoice
+    case "$ynchoice" in
+      y|Y ) printf "${GREEN}
+        Great, I will build the matrices for $NEWEST
+        This may take a bit!${NORMAL}"
+        export NEWEST
+        python3 -c "import os; from resources import processing;\
+                    processing.aggegate_likers(os.getenv('NEWEST'));\
+                    processing.aggegate_retweeters(os.getenv('NEWEST'))"
+      n|N ) printf '${RED}
+        Then please supply the path of the pull data folder:${ORANGE}
+        (use "../Pull-DD-MM-YYYY-hour:minute:second"
+        for "Pull-..." in the directory above this one):${NORMAL}'
+        read CHOICE
+        if [ -d $CHOICE ]
+        then
+        printf "${GREEN}\n        Great, I see $CHOICE\n        and will build the matrices. This may take a bit!\n"
+        export CHOICE
+        python3 -c "import os; from resources import processing;\
+                processing.aggegate_likers(os.getenv('CHOICE'));\
+                processing.aggegate_retweeters(os.getenv('CHOICE'))"
+        printf "${MAGENTA_BLINK}
+        DONE!
+        "${NORMAL}
+        else
+          printf "${RED}\n        Sorry, I can't find that directory.${NORMAL}\n"
+        fi
+      * ) printf "${RED}\n        Invalid choice, I'm afraid. Quitting...${NORMAL}\n"; exit;;
+esac
