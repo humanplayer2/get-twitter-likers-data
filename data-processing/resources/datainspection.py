05b67fb5e2e1f63924d6f2b6c3149c0021924ad8
LJ-9 <laurajahn@outlook.de>
2022-12-30 13:02:07 +0100

    Fix todos in data-processing: Main: edit notebook to include function for nicer plot for missed likers, tested

--- data-processing/resources/datainspection.py
+++ data-processing/resources/datainspection.py
@@ -12,36 +12,36 @@ from matplotlib import pyplot as plt
 
 # Absolute number of missed likes/retweets per tweet
 def plot_missed(likecount, likerscollected, retweetcount, retweeterscollected):
-
+    plt.figure(figsize=(12, 7), dpi=1000)
     fig, ax = plt.subplots(figsize =(18, 10)) 
 
-    plt.plot(list(range(0,len(likecount),1)),likecount-likerscollected, label = 'Missed likes', alpha = 0.7, linewidth = .7)
-    plt.plot(list(range(0,len(retweetcount),1)),retweetcount-retweeterscollected, label = 'Missed retweets', alpha = 0.9, linewidth = .7)
+    plt.plot(list(range(0,len(likecount),1)),likecount-likerscollected, label = 'Likes', alpha = 0.7, linewidth = .7)
+    plt.plot(list(range(0,len(retweetcount),1)),retweetcount-retweeterscollected, label = 'Retweets', alpha = 0.9, linewidth = .7)
 
 
     plt.legend(loc="upper right", prop={'size': 10})
 
     ax.set_xlabel("Tweets")
-    ax.set_ylabel('Likes/retweeters not collected')
+    ax.set_ylabel('Missed likes/retweeters')
 
     
 # Share of missed likes/retweets given total of received likes/retweets per tweet
 def plot_missed_relative(likecount, likerscollected, retweetcount, retweeterscollected):
-
+    plt.figure(figsize=(12, 7), dpi=1000)
     fig, ax = plt.subplots(figsize =(20, 10)) 
 
-    plt.plot(list(range(0,len(likecount),1)),((likecount-likerscollected)/likecount), label = 'Missed likes', alpha = 0.7, linewidth = .5)
-    plt.plot(list(range(0,len(retweetcount),1)),((retweetcount-retweeterscollected)/retweetcount), label = 'Missed retweets', alpha = 0.6, linewidth = .5)
+    plt.plot(list(range(0,len(likecount),1)),((likecount-likerscollected)/likecount), label = 'Likes', alpha = 0.7, linewidth = .5)
+    plt.plot(list(range(0,len(retweetcount),1)),((retweetcount-retweeterscollected)/retweetcount), label = 'Retweets', alpha = 0.6, linewidth = .5)
 
     plt.legend(loc="upper right", prop={'size': 10})
 
     ax.set_xlabel("Tweets")
-    ax.set_ylabel('Share of likers/retweeters per tweet not collected')
+    ax.set_ylabel('Share of missed likers/retweeters per tweet')
 
 # Supplemented with total number of likes/retweets each tweet attracted: 
 
 def plot_missed_relative_absolutecount(likecount, likerscollected, retweetcount, retweeterscollected):
-
+    plt.figure(figsize=(12, 7), dpi=1000)
     xvalretweeters = list(range(0,len(retweetcount),1))
     xvallikers= list(range(0,len(likecount),1))
 
@@ -54,10 +54,33 @@ def plot_missed_relative_absolutecount(likecount, likerscollected, retweetcount,
     ax2.plot(xvalretweeters, retweetcount, label = 'Received retweets', alpha = 0.8, color = 'tab:orange', linestyle='dotted')
     ax2.set_xlabel("Tweets")
 
-    ax1.set_ylabel('Share of likes/retweets not collected (lines)')
-    ax2.set_ylabel('Received likes/retweets (dotted)')
+    ax1.set_ylabel('Share of missed likes/retweets (lines)')
+    ax2.set_ylabel('Likes/retweets (dotted) per tweet')
 
     ax1.legend(['Retweets', 'Likes'], loc = 'upper right')#,  prop={'size': 13})
+
+def plot_missed_scatter_single(subset_maxlikes, likerscollected, label, col):
+    plt.figure(figsize=(12, 7), dpi=1000)
+    plt.rcParams.update({'font.size': 15})
+    plt.scatter(subset_maxlikes, (subset_maxlikes-likerscollected)/subset_maxlikes, alpha= 0.5, s=20, color = col, label = label)
+
+    plt.axhline(y=0, linestyle='-',linewidth = 1, color = 'grey')
+    plt.xlabel(f'{label} per tweet', labelpad=20)
+    plt.ylabel(f'Share of missed {label} per tweet', labelpad=30) 
+    plt.legend(loc='upper right')
+    plt.show()
+
+def plot_missed_scatter_combined(subset_maxlikes, likerscollected, subset_maxretweets,retweeterscollected, label1, label2, col1, col2):
+    plt.figure(figsize=(12, 7), dpi=1000)
+    plt.rcParams.update({'font.size': 15})
+    plt.scatter(subset_maxlikes, (subset_maxlikes-likerscollected)/subset_maxlikes, alpha= 0.5, s=20, color = col1, label = label1)
+    plt.scatter(subset_maxretweets, (subset_maxretweets-retweeterscollected)/subset_maxretweets, alpha= 0.5, s=20, color = col2, label = label2)
+
+    plt.axhline(y=0, linestyle='-',linewidth = 1, color = 'grey')
+    plt.xlabel(f'{label1}/{label2} per tweet', labelpad=20)
+    plt.ylabel(f'Share of missed {label1}/{label2} per tweet', labelpad=30) 
+    plt.legend(loc='upper right')
+    plt.show()
 
 
 def make_frequency_table(likers_complete, retweeters_complete):
